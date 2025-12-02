19803adee4b90dc1a3649638671e95c5be912c9c
万欣 <wanxin@feiditech.com>
2025-12-02 11:04:45 +0800

    Initial Commit

---
+++ ai-ws/ai_wsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_wsApp.swift
+//  ai-ws
+//
+//  Created by Ricardo on 2025/12/2.
+//
+
+import SwiftUI
+
+@main
+struct ai_wsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
